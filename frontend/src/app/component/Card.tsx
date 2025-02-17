bfaf7d11e49deee9230b74bfa46701a07b5dc91c
xtsulyts <531xtsulyts@gmail.com>
2025-02-17 20:49:39 -0300

    contexto solucionado, luego de login el usuario loguedo se pasa al componente mediate un hoock. Pendiente actualizar el render automaticamente con un newUser

--- frontend/src/app/component/Card.tsx
+++ frontend/src/app/component/Card.tsx
@@ -5,12 +5,12 @@ import { useUser } from "../contex/UserContex";
 
 const Card = () => {
   const { user } = useUser();
-  console.log(useUser)
+  console.log("user:", user)
 
   return (
     <div className="flex justify-center py-10">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        <h2 className="text-xl font-bold mb-4">Bienvenido, {user?.username || "Invitado"}</h2>
+        <h2 className="text-xl font-bold mb-4">Bienvenido, {user?.user}</h2>
         <p className="text-gray-700 mb-6">
           Esta es una tarjeta de ejemplo que puedes usar entre el header y el footer.
         </p>
