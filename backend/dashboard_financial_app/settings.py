1c11b7963b2fbba718495352ab1a5d3cca062bea
xtsulyts <531xtsulyts@gmail.com>
2025-12-15 11:35:35 -0300

    actualizo origin con en api con https://django-dashboard-financial.vercel.app

--- backend/dashboard_financial_app/settings.py
+++ backend/dashboard_financial_app/settings.py
@@ -61,13 +61,13 @@ INSTALLED_APPS = [
 
 CSRF_TRUSTED_ORIGINS = [
     #'http://localhost:3000',
-    "https://django-dashboard-financial.vercel.app/",
+    "https://django-dashboard-financial.vercel.app",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 
     "http://127.0.0.1:3000",
-    "https://django-dashboard-financial.vercel.app/",
+    "https://django-dashboard-financial.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
